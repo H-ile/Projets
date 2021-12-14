5ba6459af8ebb93a2d428c1befe775e97ad6d437
H_ile <achille.monteil@limayrac.fr>
2021-12-14 10:46:38 +0100

    oui

--- POO/po43/menu.php
+++ POO/po43/menu.php
@@ -7,4 +7,6 @@
   <li>Page d'<a href="index.php">accueil</a></li>
   <li>Liste des <a href="ingredient_liste.php">ingrédients</a> (API)</li>
   <li>Liste des <a href="liste_cocktail.php?lettre=a">cocktails</a> (API)</li>
+  <li>Liste des <a href="categorie.php">Catégories</a> (API)</li>
+  
 </ul>
