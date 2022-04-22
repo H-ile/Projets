fc6e5bea169f68b13f95592abaab040566b92313
H_ile <achille.monteil@limayrac.fr>
2022-04-22 10:39:55 +0200

    oui

--- POO/po22/classes/Permis.php
+++ POO/po22/classes/Permis.php
@@ -1,8 +1,15 @@
 <?php 
+// Attributs
+private $prenom;
+private $nom;
+private $nb_point = 12;
+private $type;
+private $types;
 
+ function __construct($prenom, $nom, $nb_point, $type)
+    {
+       $this -> prenom= $prenom;
+       $this -> nom= $nom;
 
-
-
-
-
+    }
 ?>
